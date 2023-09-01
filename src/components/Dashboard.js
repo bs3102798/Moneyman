38e018f6dead3e302cd039be36e6062e163a55e2
Burness Scott III <bsiii102798@gmail.com>
2023-09-01 15:05:57 -0700

    calandar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar';
 
 const Dashboard = () => {
+    const [selectedDate, setSelectedDate] = useState(null);
+
+    const handleDayClick = (date) => {
+        setSelectedDate(date);
+    };
+
     return (
         <section id="about">
             <div>
-                <h1>About Me</h1>
+                <h1>Calendar</h1>
                 <div className="box">
-                    <p>Burness Scott III is a dedicated and resilient person who puts his all in anything that he does. In his determination he learned about Programing and learned what he needed to do to further his career in the field. In doing so he learned about UCR codeing bootcamp and commited his self to learnign as much as he could. He did strugle on some project staying up late to finish them, he finished them none the less. With the jouney of the Bootcamp coming to a close the real steps towards his story in coding will soon begin.</p>
+                    <div className="calendar-container">
+                        <Calendar onClickDay={handleDayClick} value={selectedDate} />
+                    </div>
+                    <button onClick={() => setSelectedDate(null)}>Clear Highlight</button>
                 </div>
             </div>
         </section>
