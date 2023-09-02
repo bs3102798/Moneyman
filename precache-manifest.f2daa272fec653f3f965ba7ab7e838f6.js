bd4958a4f0826a69c9f66fef4e7867529abe18c3
Burness Scott III <bsiii102798@gmail.com>
2023-09-01 21:02:10 -0700

    Updates

---
+++ precache-manifest.f2daa272fec653f3f965ba7ab7e838f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab4139bc0c229aa23db",
+    "url": "/Moneyman/static/css/main.ae7518ce.chunk.css"
+  },
+  {
+    "revision": "fab4139bc0c229aa23db",
+    "url": "/Moneyman/static/js/main.fab4139b.chunk.js"
+  },
+  {
+    "revision": "b5dec1235d8d1d118e4f",
+    "url": "/Moneyman/static/js/1.b5dec123.chunk.js"
+  },
+  {
+    "revision": "a66fa942d4a6b7052bfb",
+    "url": "/Moneyman/static/js/2.a66fa942.chunk.js"
+  },
+  {
+    "revision": "78d30991565fd6d966ac",
+    "url": "/Moneyman/static/js/runtime~main.78d30991.js"
+  },
+  {
+    "revision": "decbf55d6ae2cf3eaa8e9666e0d2a135",
+    "url": "/Moneyman/index.html"
+  }
+];
