63ce0291c904bd88dda06fa383b49c59e2e01b7c
Burness Scott III <bsiii102798@gmail.com>
2023-09-02 19:43:56 -0700

    noneedjs

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/dashboard">Dashboard</Link></li>
                     <li><Link to="/dataform">dataform</Link></li>
-                    <li><Link to="/resume">Resume</Link></li>
+
                     <li><Link to="/login">Login</Link></li>
                 </ul>
             </nav>
