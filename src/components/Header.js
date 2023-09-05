fb29cb6ef8104b8a107b6de79f87739e26346af3
Aaron DeLara <aarondelara64@gmail.com>
2023-09-04 21:24:44 -0700

    nothing to see here

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,6 @@ const Header = () => {
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/dashboard">Dashboard</Link></li>
                     {/* <li><Link to="/dataform">dataform</Link></li> */}
-
                     <li><Link to="/login">Login</Link></li>
                 </ul>
             </nav>
